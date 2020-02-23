7dfa7c3d6ce7fa5f69977bad0894653b68febd1d
Stepan Morozov <morsstepan@icloud.com>
2020-02-23 23:11:39 +0300

    Cleaned up

--- src/main/java/com/morsstepan/catchmyvibe/service/SpotifyService.java
+++ src/main/java/com/morsstepan/catchmyvibe/service/SpotifyService.java
@@ -24,81 +24,11 @@ public class SpotifyService {
     @Autowired
     private SpotifyApi spotify;
 
-    public List<PlaylistSimplified> getSimplifiedPlaylists() throws IOException {
-        try {
-            int offset = 0;
-            int limit = 50;
-            Paging<PlaylistSimplified> paging;List<PlaylistSimplified> playlists = new ArrayList<>();
-
-            do {
-				paging = this.spotify.getListOfCurrentUsersPlaylists().limit(limit).offset(offset).build().execute();
-                playlists.addAll(Arrays.asList(paging.getItems()));
-                offset += limit;
-            } while (paging.getNext() != null);
-
-            return playlists;
-        } catch (SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public List<PlaylistTrack> getPlaylistTracks(String playlistId) throws IOException {
-        try {
-            int offset = 0;
-            int limit = 100;
-            Paging<PlaylistTrack> paging;
-            List<PlaylistTrack> tracks = new ArrayList<>();
-
-            do {
-                paging = this.spotify.getPlaylistsTracks(playlistId).fields("next,items(is_local,track(name, artists, is_playable))")
-                        .limit(limit).offset(offset)
-                        .market(CountryCode.CA)
-                        .build().execute();
-                tracks.addAll(Arrays.asList(paging.getItems()));
-                offset += limit;
-            } while (paging.getNext() != null);
-
-            return tracks;
-        } catch (SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<AudioFeatures> getAudioFeatures(List<String> ids) throws IOException {
-        try {
-            List<AudioFeatures> features = new ArrayList<>();
-
-            while (features.size() < ids.size()) {
-                int from = features.size();
-                List<String > subList = ids.subList(from, Math.min(ids.size(), from + 100));
-                String[] idsArr = new String[subList.size()];
-                subList.toArray(idsArr);
-                AudioFeatures[] subFeatures = this.spotify.getAudioFeaturesForSeveralTracks(idsArr).build().execute();
-                features.addAll(Arrays.asList(subFeatures));
-            }
-
-            return features;
-        } catch (SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public Playlist getPlaylist(String id) throws IOException {
-        try {
-            return this.spotify.getPlaylist(id).build().execute();
-        } catch (SpotifyWebApiException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
     public Playlist createPlaylist(Recommendations recommendations) {
         try {
             final Playlist playlist = spotify.createPlaylist(spotify.getCurrentUsersProfile().build().execute().getId(),
                     "CatchMyVibe Playlist | " + LocalDateTime.now()).build().execute();
-            spotify.changePlaylistsDetails(playlist.getId()).description("Created with <3 by CatchMyVibe");
+            spotify.changePlaylistsDetails(playlist.getId()).description("Created with <3 by CatchMyVibe").build().execute();
             spotify.addTracksToPlaylist(playlist.getId(),
                     Arrays.stream(Arrays.stream(recommendations.getTracks()).map(TrackSimplified::getUri).toArray()).toArray(String[]::new))
             .build()
